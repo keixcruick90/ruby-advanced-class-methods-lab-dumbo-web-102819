78b7ce3f083968694b15bf8296c45e5f698fe1fb
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-30 20:28:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,14 +52,8 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].append(parts[1] + ".mp3")
-
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
+    song = self.new_from_filename(filename)
+    song.save
   end
 
   def self.destroy_all
